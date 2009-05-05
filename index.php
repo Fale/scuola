ca6dcfeec228cc8766ebf453854f2bf955907715
Fabio A Locati <fale@localhost.localdomain>
2009-05-05 16:38:27 +0200

    add a "main" website

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php
+  require "template.php";
+  headers("Wiki++ - Home Page");
+?>
+
+Questo sito l'ho fatto per collezionare ed ordinare tutti gli appunti che avevo in giro del quinto anno del Liceo Scientifico Tecnologico.<br />
+Buona navigazione<br />
+Fabio A Locati
+
+<?php
+ footer();
+?>
