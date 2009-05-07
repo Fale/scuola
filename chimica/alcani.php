c9f14fb8e3dfd7b4c9b25ce3335f8adac38d6213
fale <fale@fale-laptop.(none)>
2009-05-07 16:26:03 +0200

    migliori gli alcani :)

--- chimica/alcani.php
+++ chimica/alcani.php
@@ -4,15 +4,17 @@
 ?>
 
 <h1>Alcani e i cicloalcani</h1>
-  Gli alcani e i cicloalcani sono <b>idrocarburi</b>, ossia composti che contengono soltanto carbonio e ossigeno.<br />
-  Gli alcani e i cicloalcani sono <b>saturi</b> contengono, perciò, soltanto legami semplici carbonio-carbonio.
+  <ul>
+    <li>Gli alcani e i cicloalcani sono <b>idrocarburi</b>, ossia composti che contengono soltanto carbonio e ossigeno.
+    <li>Gli alcani e i cicloalcani sono <b>saturi</b> contengono, perciò, soltanto legami semplici carbonio-carbonio.
+  </ul>
     <h4>Formula base</h4>
       C<small><sub>n</sub></small>H<small><sub>2n+2</sub></small>
   <h2>Nomenclatura</h2>
     <b>Desinenza:</b> -ano<br />
     <b>Sostituenti:</b> -ile
     <h4>Esempi</h4>
-      <table class="wikitable" width="100%" style="text-align:center">
+      <table class="wikitable" style="text-align:center">
         <tr>
           <td><b>Nome</b></td>
           <td><b>Sostituente</b></td>
@@ -26,7 +28,14 @@
           <td>Etile</td>
         </tr>
       </table>
-
+  <h2>Fonti</h2>
+    Le principali fonti naturali di lacani sono il <b>petrolio</b> e il <b>gas naturale</b>.
+  <h2>Proprietà fisiche</h2>
+    <ul>
+      <li>Gli alcani sono <b>apolari</b>, quindi <b>insolubili</b> in acqua.
+      <li>Gli alcani hanno <b>punti di ebolizione</b> più <b>bassi</b>, a parità di peso molecolare, della maggior parte degli altri composti organici.
+      <li>I punti di ebolizione degli alcani <b>crescono</b> al <i>crescere dela lunghezza della catena</b> e <i>diminuiscono se la catena si ramifica e assume una forma più simile a quella sferica.
+    </ul>
 <?php
  footer();
 ?>
