3f98fa78e9fd3801c7130751c11fe75e6b689998
fale <fale@fale-laptop.(none)>
2009-05-07 16:36:57 +0200

    miglior stile negli isomeri :)

--- chimica/isomeri.php
+++ chimica/isomeri.php
@@ -2,17 +2,17 @@
   require "template.php";
   headers("AppuntiEsame - Chimica Organica - Gruppi");
 ?>
-
+<h1>Tipi di isomeri</h1>
 <table class="wikitable" width="100%" style="text-align:center">
   <tr>
-    <td colspan="2">Isomeri di struttura (o di posizione)<br /><i>legami differenti</i></td>
+    <td colspan="2"><b>Isomeri di struttura</b> (o di posizione)<br /><i>legami differenti</i></td>
   </tr>
   <tr>
-    <td rowspan="2">Stereoisomeri<br /><i>stessi legami</i></td>
-    <td>Conformeri (rotameri)<br /><i>interconvertibili per<br />rotazione attorno a un legame</i></td>
+    <td rowspan="2"><b>Stereoisomeri</b><br /><i>stessi legami</i></td>
+    <td><b>Conformeri</b> (rotameri)<br /><i>interconvertibili per<br />rotazione attorno a un legame</i></td>
   </tr>
   <tr>
-    <td>Isomeri configurazionali<br /><i><b>non</b> interconvertibili per<br />rotazione attorno a un legame</i></td>
+    <td><b>Isomeri configurazionali</b><br /><i><b>non</b> interconvertibili per<br />rotazione attorno a un legame</i></td>
   </tr>
 </table>
 
